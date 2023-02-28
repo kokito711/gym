348d03fb775984c6c73920242a1a6fa879670d24
Sergio <serlopjim91@gmail.com>
2023-02-28 23:48:42 +0100

    initial commit
    Signed-off-by: Thekoko <avasir2slj@gmail.com>

---
+++ src/main/java/sergio/pruebas/gym/GymApplication.java
@@ -0,0 +1,13 @@
+package sergio.pruebas.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApplication.class, args);
+	}
+
+}
